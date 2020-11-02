81beb16f50330d548d605a2b759cb495e4c6e7a9
ZacharyRogers <ztrogers1@gmail.com>
2020-11-01 20:15:53 -0800

    R - Adding aws configuration to prod environment

--- WebApplication/personal-website/src/environments/environment.prod.ts
+++ WebApplication/personal-website/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  name: 'prod',
+  awsConfig: {
+    Auth: {
+      identityPoolId: 'us-east-1:e0e9ebb6-21c3-4330-b8b9-d4c2f307275c',
+      region: 'us-east-1',
+      userPoolId: 'us-east-1_bRAPsGeo2',
+      userPoolWebClientId: '7isv2q0ebte5e73ri63ih408rl'
+    }
+  }
 };
+
