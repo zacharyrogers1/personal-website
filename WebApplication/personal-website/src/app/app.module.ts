56f50281e64e89751d9742dc8f02da1136623367
ZacharyRogers <ztrogers1@gmail.com>
2020-11-15 18:33:19 -0800

    F - default page is no longer home

--- WebApplication/personal-website/src/app/app.module.ts
+++ WebApplication/personal-website/src/app/app.module.ts
@@ -17,8 +17,7 @@ Amplify.configure(environment.awsConfig);
 const routes: Routes = [
   {path: 'demo', loadChildren: ()=> import('./modules/demo/demo.module').then(m => m.DemoModule)},
   {path: 'signIn', loadChildren: ()=> import('./modules/sign-in/sign-in.module').then(m => m.SignInModule)},
-  {path: 'home', loadChildren: ()=> import('./modules/main/main.module').then(m => m.MainModule)},
-  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: '', loadChildren: ()=> import('./modules/main/main.module').then(m => m.MainModule)},
   {path: '**', loadChildren: 'home'}
 ];
 
