3d9afabcfbabb749d6415dcb2d2ccfd2e1bce17f
ZacharyRogers <ztrogers1@gmail.com>
2021-03-14 15:05:48 -0400

    deleted comments

--- Infrastructure/serverless/websockets/sharedFunctions/websocketCommunicationHelper.ts
+++ Infrastructure/serverless/websockets/sharedFunctions/websocketCommunicationHelper.ts
@@ -19,20 +19,3 @@ export interface ISendMessageParams {
   connectionId: string,
   message: string
 }
-
-
-
-
-
-
-
-// export const executeSend = (ws, { domainName, stage, connectionId, message }) => {
-//   ws.endpoint = `${domainName}/${stage}`;
-
-//   const postParams = {
-//     Data: message,
-//     ConnectionId: connectionId
-//   };
-
-//   return ws.postToConnection(postParams).promise();
-// };
