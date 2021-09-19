ed057081e91590b90272571396f257590f1d8442
ZacharyRogers <ztrogers1@gmail.com>
2021-09-19 15:05:36 -0400

    Made pixelPaint dynamically resize based on window size

--- WebApplication/personal-website/src/app/modules/light-array/pixel-paint/pixel-paint.component.ts
+++ WebApplication/personal-website/src/app/modules/light-array/pixel-paint/pixel-paint.component.ts
@@ -1,6 +1,6 @@
 import { AfterViewInit, ChangeDetectionStrategy, ChangeDetectorRef, Component, OnDestroy, OnInit } from '@angular/core';
 import { fromEvent, Observable, Subject, Subscription } from 'rxjs';
-import { catchError, distinctUntilChanged, filter, map, tap } from 'rxjs/operators';
+import { catchError, debounceTime, distinctUntilChanged, filter, map, tap } from 'rxjs/operators';
 import { MqttService } from 'src/app/services/mqtt.service';
 import { LightArrayService } from '../light-array.service';
 import { IColorTile, IPaintPixel, RgbScreen } from '../types';
@@ -19,19 +19,34 @@ export class PixelPaintComponent implements OnInit, OnDestroy, AfterViewInit {
   pixelPaintColor = 'rgb(0,0,0)';
   private readonly grey: string = 'rgb(200,200,200)';
   subscriptions: Subscription[] = [];
-  isFullScreen:boolean = false;
-  colorStream: Subject<{x:number, y:number}> = new Subject();
-  colorStreamEffects: Observable<any> = this.colorStream.pipe(
-    map(({x, y}) =>  document.elementsFromPoint(x, y)[1].id),//THe 0th element is a mat figure and we need the mat-tile element),
-    filter((id:string) => id && (id !== 'paintTiles')),
+  isFullScreen: boolean = false;
+  colorStream: Subject<{ x: number, y: number }> = new Subject();
+  colorStreamEffects$: Observable<any> = this.colorStream.pipe(
+    map(({ x, y }) => document.elementsFromPoint(x, y)[1].id),//THe 0th element is a mat figure and we need the mat-tile element),
+    filter((id: string) => id && (id !== 'paintTiles')),
     distinctUntilChanged(),
-    tap((id:string) => {
+    tap((id: string) => {
       this.tilesToDisplay[id].color = this.pixelPaintColor;
       this.changeDetectorRef.detectChanges()
       this.publishToPixelPaint(parseInt(id), this.pixelPaintColor);
     }),
     catchError((err, caught) => caught)
   );
+  resizePixelPaint$: Observable<any> = fromEvent(window, 'resize').pipe(
+    tap((event) => {
+      const paintContainer = document.getElementById('paintTiles');
+      if (paintContainer) {
+        const matCardMarginSize = 16;
+        const matCardTitleHeight = 31;
+        const matCardTitlePadding = 8;
+        const actualWidth: number = event.target.innerWidth - (matCardMarginSize * 2)
+        const actualHeight: number = event.target.innerHeight - (matCardMarginSize * 2) - matCardTitleHeight - matCardTitlePadding
+        const smallerValue: number = actualWidth >= actualHeight ? actualHeight : actualWidth;
+        paintContainer.style.height = smallerValue + 'px';
+        paintContainer.style.width = smallerValue + 'px';
+      }
+    })
+  )
 
   constructor(
     private lightArrayService: LightArrayService,
@@ -44,11 +59,11 @@ export class PixelPaintComponent implements OnInit, OnDestroy, AfterViewInit {
     for (let i = 0; i < this.pixelCount; i++) {
       this.tilesToDisplay.push({ displayName: i.toString(), index: i, color: this.grey });
     }
-    this.subscriptions.push(
-      this.colorStreamEffects.subscribe()
-    );
+    this.subscriptions.push(this.colorStreamEffects$.subscribe());
+
+    this.subscriptions.push(this.resizePixelPaint$.subscribe());
   }
-  
+
   ngAfterViewInit() {
     this.changeDetectorRef.detectChanges()
   }
@@ -56,7 +71,7 @@ export class PixelPaintComponent implements OnInit, OnDestroy, AfterViewInit {
   toggleFullScreen() {
     this.isFullScreen = !this.isFullScreen;
     const pixelPaintElement = document.getElementById("pixelPaintContainer")
-    if(!document.fullscreenElement){
+    if (!document.fullscreenElement) {
       pixelPaintElement.requestFullscreen().then(() => {
         this.changeDetectorRef.detectChanges()
       });
@@ -73,7 +88,7 @@ export class PixelPaintComponent implements OnInit, OnDestroy, AfterViewInit {
   }
 
   panOverTile(evt: any) {
-    this.colorStream.next({x: evt.center.x, y:  evt.center.y})
+    this.colorStream.next({ x: evt.center.x, y: evt.center.y })
   }
 
   publishToPixelPaint(index: number, color: string) {
